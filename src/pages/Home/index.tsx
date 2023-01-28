732683ef6365a27d6c08ccccc8b163c5c4055990
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-01-28 09:14:10 -0300

    added styles from home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,58 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestion"
+            type="text"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestion">
+            <option value="projeto 1" />
+            <option value="projeto 2" />
+          </datalist>
+
+          <label htmlFor="minutsAmount">Durante</label>
+          <MinutesAmountInput
+            id="minutsAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountDownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  )
+}
